402147279f7113ab431ba5b937653ce9a30470e5
sprhawk <hongbo@yang.me>
2015-03-18 12:29:43 +0800

    Initial Commit

---
+++ yble/yble.h
@@ -0,0 +1,13 @@
+//
+//  yble.h
+//  yble
+//
+//  Created by YANG HONGBO on 2015-3-18.
+//  Copyright (c) 2015年 Yang.me. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface yble : NSObject
+
+@end
---
+++ yble/yble.m
@@ -0,0 +1,13 @@
+//
+//  yble.m
+//  yble
+//
+//  Created by YANG HONGBO on 2015-3-18.
+//  Copyright (c) 2015年 Yang.me. All rights reserved.
+//
+
+#import "yble.h"
+
+@implementation yble
+
+@end
